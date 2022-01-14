10ee5c138282779bb87499927615887f37829169
Heshan <hrathnapala@gmail.com>
2022-01-14 21:59:32 +0530

    added dependency and testing route for dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { chats } = require("./data/data");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Invalid Endpoint");
 });
 
-app.listen(5000, () => {
-  console.log("Server Started on PORT " + 5000);
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const chat = chats.find((c) => c._id === req.params.id);
+  res.send(chat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server Started on PORT " + PORT);
 });
